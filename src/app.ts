79ca4331c6e9dbe2fc10a537fdb965b539962428
Jordan-182 <Jordan.P62@outlook.fr>
2026-07-10 10:33:51 +0200

    feat: add UNIQUE constraints in database and remove useless test routes from app

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,10 @@
-import express, { type Express, type Request, type Response } from "express";
-import { pool } from "./db.ts";
+import express, { type Express } from "express";
 import beerRoutes from "./routes/beerRoutes.ts";
 
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
-
-app.get("/api", (_req: Request, res: Response) => {
-  res.send("Hello Zythologue!");
-});
-
-app.get("/api/health/db", async (_req: Request, res: Response) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json({ status: "ok", time: result.rows[0].now });
-  } catch (err) {
-    res.status(500).json({ status: "error", message: (err as Error).message });
-  }
-});
 
 app.use("/api/beers", beerRoutes);
 
